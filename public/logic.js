17bb7ab28aabb0e034f1d53e6201b87717375390
James Daniels <jrd656@gmail.com>
2020-04-14 15:59:14 +0300

    hadi update

--- public/logic.js
+++ public/logic.js
@@ -7,8 +7,6 @@ var url = '/dictfile';
 xhr.open('GET', url);
 xhr.send();
 
-console.log(xhr.readyState)
-
 xhr.onreadystatechange = function onReadyStateChange() {
   console.log("xhr fired")
   if (xhr.readyState === 4 && xhr.status === 200) {
@@ -17,4 +15,32 @@ xhr.onreadystatechange = function onReadyStateChange() {
     console.log(response[4])
   }
 };
+
+function clearSearchList(){
+  const list = document.getElementById('suggestions')
+
+  Array.from(list.childNodes).forEach(node => {
+    list.removeChild(node);
+  });
+}
+
+function setDataList(optionValues){
+  const list = document.getElementById('suggestions') 
+
+  optionValues.forEach(word=>{
+    const optionNode = document.createElement('option')
+    optionNode.value = word;
+    list.appendChild(optionNode)
+  })
+}
+
+
+searchInput.addEventListener('input', ()=>{
+  clearSearchList();
+  setDataList(searchInput.value)
+})
+
+
+
+
 
